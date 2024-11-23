314b0f8d5ffd06b28bff09bdb36b1ce42c9c32a2
iariass <iariass@usm.cl>
2024-11-22 21:23:27 -0300

    creacion de inicio de sesion

--- certamen3/urls.py
+++ certamen3/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from core.views import home, iniciarSesion
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home, name="home" ),
+    path('login/', iniciarSesion, name="login"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+import re
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
     return render(request, "core/index.html")
+
+def iniciarSesion(request):
+    data = {
+        'form':UserCreationForm
+    }
+   
+    username = request.POST.get('username', '')
+    password1 = request.POST.get('password1', '')
+    flag = True
+
+    
+    if(request.method == 'GET'):
+        return render(request, 'core/login.html',data)
+    else:
+        #################### VALIDACIONES ##########################
+        if(len(username) == 0 or len(password1) == 0):
+            flag = False
+        ############################################################
+        print(flag)
+        if(flag):
+                
+            print(request.POST)
+            user = authenticate(request, username= request.POST['username'], password=request.POST['password1'])
+        
+            if user is None:
+                data = {
+                    'form':UserCreationForm,
+                    'error1': ' usuario o contraseña incorrectas'
+                }
+            else:
+                login(request, user)
+                return redirect(home)
+        else:
+            data = {
+                'form':UserCreationForm,
+                'error2':' debe ingresar informacion en ambos campos'
+            }
+    return render(request, 'core/login.html', data) 
