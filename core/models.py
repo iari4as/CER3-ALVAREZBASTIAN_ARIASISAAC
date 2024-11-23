a20c671316dd2521a6d6625b07e777cc2e60668d
BalvarezSoto <balvarez@usm.cl>
2024-11-22 21:48:59 -0300

    Creacion del modelo EventoAcademico

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class EventoAcademico(models.Model):
+    TiposEventos = [
+        ('exam', 'Examen'),
+        ('holiday', 'Feriado'),
+        ('admin', 'Administrativo'),
+    ]
+    Titulo = models.CharField(max_length=200)
+    Descripcion = models.TextField()
+    InicioPeriodo = models.DateField()
+    FinPeriodo = models.DateField()
+    TipoEvento = models.CharField(max_length=50, choices=TiposEventos)
+
+    def __str__(self):
+        return self.title
