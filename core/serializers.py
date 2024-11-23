c263036c2eead801b8410ef1f63bb7c1d891ec3c
BalvarezSoto <balvarez@usm.cl>
2024-11-22 22:06:08 -0300

    Se crea la serializacion del modelo a JSON

---
+++ core/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import EventoAcademico
+
+class AcademicEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventoAcademico
+        fields = '__all__'
+
+    def validate(self, data):
+        if data['InicioPeriodo'] > data['FinPeriodo']:
+            raise serializers.ValidationError("La fecha de inicio debe ser anterior a la fecha de fin.")
+        return data
