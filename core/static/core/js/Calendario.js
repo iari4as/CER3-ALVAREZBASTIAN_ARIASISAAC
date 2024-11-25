6e9a73edad5c2409d117c7c6c89a6563612ad1d5
BalvarezSoto <balvarez@usm.cl>
2024-11-25 01:57:00 -0300

    Feriados agregados al calendario

--- core/static/core/js/Calendario.js
+++ core/static/core/js/Calendario.js
@@ -1,29 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Seleccionamos el elemento donde se renderizará el calendario
     const calendarEl = document.getElementById('calendar');
 
-    // Inicializamos el calendario
     const calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth', // Vista inicial
-        locale: 'es', // Idioma del calendario
+        initialView: 'dayGridMonth',
+        locale: 'es',
         headerToolbar: {
             left: 'prev,next today',
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
-        events: '/get-events/', // Ruta que devuelve los eventos en formato JSON
+        events: '/get-events/', // URL que devuelve eventos y feriados
         eventClick: function (info) {
-            // Muestra los detalles del evento al hacer clic en él
+            // Mostrar detalles del evento/feriado
             alert(`Título: ${info.event.title}\nDescripción: ${info.event.extendedProps.description}`);
-        },
-        loading: function (isLoading) {
-            // Muestra un mensaje de carga si es necesario
-            if (isLoading) {
-                console.log("Cargando eventos...");
-            }
         }
     });
 
-    // Renderizamos el calendario
     calendar.render();
 });
